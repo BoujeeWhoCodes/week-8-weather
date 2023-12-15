744e363f3a798e1c93fa1fba2d3f2232323754c2
BoujeeWhoCodes <145911615+BoujeeWhoCodes@users.noreply.github.com>
2023-12-15 23:27:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -69,22 +69,26 @@ function getForecast(city) {
 function displayForecast(response) {
   let forecastHtml = "";
 
-response.data.daily.forEach(function (day, index) {
+  response.data.daily.forEach(function (day, index) {
     if (index < 5) {
       forecastHtml =
         forecastHtml +
-      `
+        `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${formatDay(day.time)}<div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
         <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
             <strong>${Math.round(day.temperature.maximum)}º</strong>
           </div>
-          <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
         </div>
       </div>
+         </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
