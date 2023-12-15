6fa9fd6259f4e305cd7e7de0e7f5cc8942e7664a
BoujeeWhoCodes <145911615+BoujeeWhoCodes@users.noreply.github.com>
2023-12-15 15:06:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -69,13 +69,13 @@ function displayForecast() {
       forecastHtml +
       `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}<div>
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
           </div>
-          <div class="weather-forecast-temperature">9º</div>
+          <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
         </div>
       </div>
     `;
